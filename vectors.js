31fa173c8f322ae404bad0557a3e7e8c5ec186a9
Pinheiroiam <66079009+Pinheiroiam@users.noreply.github.com>
2020-06-17 01:13:00 -0300

    Update vectors.js

--- vectors.js
+++ vectors.js
@@ -17,12 +17,14 @@ const developer = [
     }
 ]
 
-console.log(`O desenvolvedor ${developer[0].name} tem ${developer[0].age} 
-anos e usa as tecnologias: ${developer[0].technology[0].name} 
-com especialidade em ${developer[0].technology[0].especialty} e ${developer[0].technology[1].name} 
-com especialidade em ${developer[0].technology[1].especialty}`);
+console.log(
+    `O desenvolvedor ${developer[0].name} tem ${developer[0].age} anos e usa as tecnologias: 
+    ${developer[0].technology[0].name} com especialidade em ${developer[0].technology[0].especialty} e 
+    ${developer[0].technology[1].name} com especialidade em ${developer[0].technology[1].especialty}`
+);
 
-console.log(`O desenvolvedor ${developer[1].name} tem ${developer[1].age} 
-anos e usa as tecnologias: ${developer[1].technology[0].name} 
-com especialidade em ${developer[1].technology[0].especialty} e ${developer[1].technology[1].name} 
-com especialidade em ${developer[1].technology[1].especialty}`);
+console.log(`
+    O desenvolvedor ${developer[1].name} tem ${developer[1].age} anos e usa as tecnologias: 
+    ${developer[1].technology[0].name} com especialidade em ${developer[1].technology[0].especialty} e 
+    ${developer[1].technology[1].name} com especialidade em ${developer[1].technology[1].especialty}`
+);
